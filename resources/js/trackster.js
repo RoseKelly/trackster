22a6f2577b73d1c5a788e88d1043327104c2ec75
Rose Kelly <lrkelly09@gmail.com>
2017-05-22 07:48:20 -0500

    project finish

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -1,11 +1,38 @@
 var Trackster = {};
 
+$(document).ready(function() {
+  $('#search-button').click(function() {
+    Trackster.searchTracksByTitle($('#search-input').val());
+  });
+});
+
+
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks. 
 */
 Trackster.renderTracks = function(tracks) {
+  var $trackList = $('#track-list');
 
+  $trackList.empty();
+
+  for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    var track = tracks[trackIndex];
+    var htmlTrackRow =
+      '<div class="row track">' +
+      '  <div class="col-xs-1 col-xs-offset-1 play-button">' +
+      '    <a href="' + track.preview_url + '" target="_blank">' +
+      '      <i class="fa fa-play-circle-o fa-2x"></i>' +
+      '    </a>' +
+      '  </div>' +
+      '  <div class="col-xs-4">' + track.name + '</div>' +
+      '  <div class="col-xs-2">' + track.artists[0].name + '</div>' +
+      '  <div class="col-xs-2">' + track.album.name + '</div>' +
+      '  <div class="col-xs-2">' + track.popularity + '</div>' +
+      '</div>';
+
+    $trackList.append(htmlTrackRow);
+  }
 };
 
 /*
@@ -13,5 +40,10 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+  $.ajax({
+    url: 'https://api.spotify.com/v1/search?type=track&q=' + title,
+    success: function(response) {
+      Trackster.renderTracks(response.tracks.items);
+    }
+  });
 };
